ee51a19b2df7b829f5d8cf871f18aba1c732b399
David Lee <DavidLee8two7@gmail.com>
2018-06-10 20:58:24 -0700

    Events & effects

--- Basic setup/main.js
+++ Basic setup/main.js
@@ -1,4 +1,11 @@
 $('.fade, .fades, #fade, #fades').on('click', function() {
-  $(this).toggleClass('red');
-  $(this).fadeOut(3000);
+  // $(this).toggleClass('red');
+  // $(this).fadeOut(3000);
+  // $('#box').toggle();
+  // $('#box').slideDown(2000);
+  // $('#box').slideUp();
+  // $('#box').fadeIn();
+  // $('#box').fadeOut();
+  // $('#box').fadeTo(1000, 0.5);
+  // $('#box').fadeToggle(1000);
 });
